e26f9076168f220008bbd5bc398b985ff8c60839
Yi-Mu Chen <enochnotsocool@gmail.com>
2021-12-01 15:01:42 -0500

    Formally add the zscan command documentation

--- InvSqCalc/bin/InvSq_ZScanPlot.cc
+++ InvSqCalc/bin/InvSq_ZScanPlot.cc
@@ -1,3 +1,36 @@
+/**
+ * @file InvSq_ZScanPlot.cc
+ * @brief Command `SiPM_InvSq_ZScanPlot`: Running the inverse square fit a data
+ * file collected with z scan.
+ *
+ * @details
+ * This function expects the a standard data format, collection via a zscan
+ * command with a singular power setting. The data columns are expected to
+ * represent the readout value and the uncertainty in the readout value (poisson
+ * uncertainty already reduced), this command then performs a inverse square fit
+ * to the data, and generates a plot displaying the full output. A summary of the
+ * fit results in text format is also displayed on the terminal for rapid
+ * debugging.
+ *
+ * ### List of command specific options:
+ *
+ * - `--data, -d`: The path to the input data file.
+ * - `--output, -o`: The path to store the output plot file (PDF).
+ * - `--uncscale,　-u`:　Scale factor to apply to the uncertainty column, this is
+ *   in place in case you which to run the fit without the Poisson uncertainty
+ *   subtraction, or which to modify the uncertainty readout in any way.
+ *
+ * ### Additional details:
+ *
+ * Notice that the plot generated doesn't place the data at the original
+ * coordinate position and the origian readout luminosity values. As for the
+ * perfect fit with an inverse square, the reader will typically expected that
+ * the fit result in the log-log plot is a straight line, which the human eye is
+ * very sensitive at picking up deviations from. For this reason, we shift the
+ * data to remove the effects of pedestal and z directional offset so that the
+ * data presented be a straight line. The removal of these effects are still
+ * explicitly spelt out in the axis labels.
+ */
 #include "SiPMCalib/Common/interface/StdFormat.hpp"
 
 #include "UserUtils/Common/interface/ArgumentExtender.hpp"
@@ -8,6 +41,14 @@
 #include "TFitResult.h"
 #include "TGraphErrors.h"
 
+/**
+ * @brief The inverse square law formula with just the z coordinate as the
+ * variable.
+ *
+ * Notice the value of N0 is used to scale the fit function by some constant
+ * amount. As this parameter has perfect correlation with N, this parameter
+ * should only ever be set as a constant.
+ */
 double
 ZProf( const double* vz, const double* param )
 {
@@ -23,25 +64,27 @@ ZProf( const double* vz, const double* param )
   return ( N*N0*( z-z0 ) )/( D2 * sqrt( D2 ) ) + P;
 }
 
-TGraph*     MakeZScanGraph( const StdFormat&, double uncscale );
-TFitResult* FitAndShiftData( TGraph* data, TF1* func,
-                             double& ped, double& zoffset );
+TGraphErrors MakeZScanGraph( const StdFormat&, double uncscale );
 
+TFitResult   FitAndShiftData( TGraph& data, TF1& func,
+                              double& ped, double& zoffset );
+
+/**
+ * @brief Main routine for plotting inverse z scan. Go to the source file for
+ * detailed documentation.
+ */
 int
 main( int argc, char** argv )
 {
   usr::po::options_description desc( "Options for plot making" );
   desc.add_options()
-    ( "data,d", usr::po::value<std::string>(), "Data file" )
-    ( "output,o", usr::po::value<std::string>(), "Output plot file" )
-    ( "norm,n", usr::po::defvalue<double>( 1.0 ),
-    "Estimated norm in the inverse square formula, this helps with the "
-    "convergence of fitter" )
-    ( "uncscale,u", usr::po::defvalue<double>( 1 ),
-    "Additional uncertainty scaling factor" )
+    ( "data,d", usr::po::reqvalue<std::string>(), "Data file" )
+    ( "output,o", usr::po::reqvalue<std::string>(), "Output plot file" )
+    ( "uncscale,u", usr::po::defvalue<double>( 1 ), "Additional uncertainty scaling factor" )
   ;
 
   usr::ArgumentExtender arg;
+  arg.AddVerboseOpt();
   arg.AddOptions( desc );
   arg.ParseOptions( argc, argv );
 
@@ -49,35 +92,37 @@ main( int argc, char** argv )
   double zoffset  = 0;  // For storing the original fit results
 
   StdFormat data( arg.Arg( "data" ) );
-  TGraph* dataz     = MakeZScanGraph( data, arg.Arg<double>( "uncscale" ) );
+  TGraph dataz      = MakeZScanGraph( data, arg.Arg<double>( "uncscale" ) );
   const double xmin = usr::plt::GetXmin( dataz );
   const double xmax = usr::plt::GetXmax( dataz );
-  TF1* func         = new TF1( "func", ZProf, xmin, xmax, 5 );
-  func->FixParameter( 4, arg.Arg<double>( "norm" ) );
-  TFitResult* fit = FitAndShiftData( dataz, func, pedestal, zoffset );
 
+  // Making the data
+  TF1 func( "func", ZProf, xmin, xmax, 5 );
+  const TFitResult fit = FitAndShiftData( dataz, func, pedestal, zoffset );
   // Begin plotting
   usr::plt::Ratio1DCanvas c;
   auto& fitg = c.PlotFunc( func,
     usr::plt::PlotType( usr::plt::fittedfunc ),
-    usr::plt::VisualizeError( fit ),
     usr::plt::EntryText( "Fitted Data" ),
+    usr::plt::VisualizeError( fit ),
+    usr::plt::FillColor( usr::plt::col::cyan ),
     usr::plt::TrackY( usr::plt::tracky::max ),
-    usr::plt::LineColor( usr::plt::col::blue ),
-    usr::plt::FillColor( usr::plt::col::cyan ) );
+    usr::plt::LineColor( usr::plt::col::blue )
+    );
   auto& datag = c.PlotGraph( dataz,
     usr::plt::PlotType( usr::plt::scatter ),
     usr::plt::EntryText( "Readout" ),
     usr::plt::MarkerColor( usr::plt::col::black ),
     usr::plt::MarkerStyle( usr::plt::sty::mkrcircle ),
-    usr::plt::MarkerSize( 0.3 ),
-    usr::plt::LineColor( usr::plt::col::gray, 0.5 ) );
+    usr::plt::MarkerSize( 0.2 ),
+    usr::plt::LineColor( usr::plt::col::gray, 1.0 ) );
   c.PlotScale( fitg, fitg,
     usr::plt::PlotType( usr::plt::fittedfunc ),
     usr::plt::FillColor( usr::plt::col::cyan ) );
   c.PlotScale( datag, fitg,
     usr::plt::PlotType( usr::plt::scatter ),
-    usr::plt::MarkerSize( 0.3 ) );
+    usr::plt::MarkerSize( 0.2 ) );
+
 
   c.TopPad().Yaxis().SetTitle( "Luminosity - Ped. [mV-ns]" );
   c.BottomPad().Xaxis().SetTitle( "Gantry z + z_{0} [mm]" );
@@ -94,22 +139,20 @@ main( int argc, char** argv )
   .SetTextCursor( c.TopPad().InnerTextLeft(), 0.45 )
   .WriteLine( "#frac{L_{0}(z+z_{0})}{(x_{0}^{2} + (z+z_{0})^{2})^{3/2}} + Ped" )
   // .WriteLine( "" )
-  .WriteLine( ( boost::format( "Fit Ped = %.2lf_{#pm%.3lf}" )
-                % pedestal % func->GetParError( 3 ) ).str() )
-  .WriteLine( ( boost::format( "Fit z_{0} = %.2lf_{#pm%.3lf}" )
-                % -zoffset % func->GetParError( 0 ) ).str() )
-  .WriteLine( ( boost::format( "Fit x_{0} = %.2lf_{#pm%.3lf}" )
-                % func->GetParameter( 1 ) % func->GetParError( 1 ) ).str() )
-  .WriteLine( ( boost::format( "#chi^{2}/D.o.F = %.2lf" )
-                % ( fit->Chi2()/fit->Ndf() ) ).str() );
+  .WriteLine( usr::fstr( "Fit Ped = %.2lf_{#pm%.3lf}",  pedestal, func.GetParError( 3 ) ) )
+  .WriteLine( usr::fstr( "Fit z_{0} = %.2lf_{#pm%.3lf}", -zoffset, func.GetParError( 0 ) ) )
+  .WriteLine( usr::fstr( "Fit x_{0} = %.2lf_{#pm%.3lf}", func.GetParameter( 1 ), func.GetParError( 1 ) ) )
+  .WriteLine( usr::fstr( "#chi^{2}/D.o.F = %.2lf/%d", fit.Chi2(), fit.Ndf() ) );
 
   c.SaveAsPDF( arg.Arg( "output" ) );
 
   return 0;
 }
 
-
-TGraph*
+/**
+ * @brief Morphing the standard format into a ROOT TGraph container.
+ */
+TGraphErrors
 MakeZScanGraph( const StdFormat& sformat, const double uncscale )
 {
   std::vector<double> z       = sformat.Z();
@@ -124,14 +167,27 @@ MakeZScanGraph( const StdFormat& sformat, const double uncscale )
     return x * uncscale;
   } );
 
-  return new TGraphErrors( z.size(),
+  return TGraphErrors( z.size(),
     z.data(), lumi.data(),
     zero.data(), lerr_scaled.data() );
 }
 
-
-TFitResult*
-FitAndShiftData( TGraph* data, TF1* func, double& ped, double& zoffset )
+/**
+ * @brief Fitting the data to the inverse square function and shift the data
+ * points in the data graph to remove pedestal and vertical offset effect.
+ *
+ * The fit will be performed a first time to determined the pedestal and vertical
+ * off set. Then, the data will be shifted according to the fitted pedestal and
+ * vertical positions. Here we also scale the luminosity normalization such that
+ * the fit uncertainty on the luminosity should be the same as the vertical
+ * offset uncertainty. The pedestal and vertical offset will be stored in the
+ * latter input inputs.
+ *
+ * Once the data shifting is complete, we then perform the fit again such that
+ * the function parameters properly match the data.
+ */
+TFitResult
+FitAndShiftData( TGraph& data, TF1& func, double& ped, double& zoffset )
 {
   usr::log::PrintLog( usr::log::INFO,
     "Getting the estimate values for fitting" );
@@ -139,37 +195,52 @@ FitAndShiftData( TGraph* data, TF1* func, double& ped, double& zoffset )
   const double lumimin = usr::plt::GetYmin( data );
   const double zmin    = usr::plt::GetXmin( data );
 
-  func->SetParameter( 0, 0 );
-  func->SetParameter( 1, 0 );
-  func->SetParameter( 2, ( lumimax*zmin*zmin )/func->GetParameter( 4 ) );
-  func->SetParameter( 3, lumimin );
+  func.SetParameter( 0, 5 );
+  func.SetParameter( 1, 0 );
+  func.SetParameter( 2, ( lumimax*( zmin+5 )*( zmin+5 ) ) );
+  func.SetParameter( 3, lumimin );
+  func.FixParameter( 4, 1.0 );
 
   usr::log::PrintLog( usr::log::INFO, "Running the fit" );
-  data->Fit( func, "Q EX0 M E N 0" ).Get();
+  data.Fit( &func, "Q EX0 M E N 0" ).Get();
 
   // Saving the original fit results for the pedestal and zoffset
-  ped     = func->GetParameter( 3 );
-  zoffset = func->GetParameter( 0 );
+  ped     = func.GetParameter( 3 );
+  zoffset = func.GetParameter( 0 );
+  const double scale = func.GetParError( 0 ) / func.GetParError( 2 );
+  func.FixParameter( 4, 1/ scale  );
+  func.SetParameter( 2, func.GetParameter( 2 ) *  scale );
 
   usr::log::PrintLog( usr::log::INFO,
     "Shifting the data for better plotting" );
 
-  for( int j = 0; j < data->GetN(); ++j ){
-    data->GetX()[j] -= zoffset;
-    data->GetY()[j] -= ped;
+  for( int j = 0; j < data.GetN(); ++j ){
+    data.GetX()[j] -= zoffset;
+    data.GetY()[j] -= ped;
   }
 
   usr::log::PrintLog( usr::log::INFO,
     "Shifting the function for better plotting" );
-  func->SetParameter( 0, 0.0 );
-  func->SetRange( usr::plt::GetXmin( data ), usr::plt::GetXmax( data ) );
+  func.SetParameter( 0, 0.0 );
+  func.SetRange( usr::plt::GetXmin( data ), usr::plt::GetXmax( data ) );
 
   usr::log::PrintLog( usr::log::INFO,
     "Refitting to get a correct result container" );
-  TFitResult* ans = data->Fit( func, "Q EX0 M E N 0 S" ).Get();
+  TFitResult* ans = data.Fit( &func, "Q EX0 M E N 0 S" ).Get();
 
   usr::log::PrintLog( usr::log::INFO,
     "Complete fitting routine" );
 
-  return ans;
+  usr::log::PrintLog( usr::log::INFO,
+    usr::fstr( "Fitted z0: %.2lf +- %.2lf", zoffset, func.GetParError( 0 ) ) );
+  usr::log::PrintLog( usr::log::INFO,
+    usr::fstr( "Fitted x0: %.2lf +- %.2lf", func.GetParameter( 1 ), func.GetParError( 1 ) ) );
+  usr::log::PrintLog( usr::log::INFO,
+    usr::fstr( "Fitted L0: %.2lf +- %.2lf", func.GetParameter( 2 ), func.GetParError( 2 ) ) );
+  usr::log::PrintLog( usr::log::INFO,
+    usr::fstr( "Fitted ped: %.2lf +- %.2lf", ped, func.GetParError( 3 ) ) );
+
+  ans->Print();
+
+  return *ans;
 }
