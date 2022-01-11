ffe287e0d6a55a53ca0c135523ac38afcb491c01
Yi-Mu Chen <enochnotsocool@gmail.com>
2022-01-11 09:48:00 -0500

    Added detailed documentation to nonlinear fitting routine class

--- SiPMCalc/src/SiPMNonLinearFit.cc
+++ SiPMCalc/src/SiPMNonLinearFit.cc
@@ -5,23 +5,64 @@
 
 #include "UserUtils/Common/interface/Maths.hpp"
 #include "UserUtils/Common/interface/STLUtils.hpp"
+#include "UserUtils/PlotUtils/interface/Flat2DCanvas.hpp"
 #include "UserUtils/PlotUtils/interface/PlotCommon.hpp"
 #include "UserUtils/PlotUtils/interface/Ratio1DCanvas.hpp"
+#include "UserUtils/PlotUtils/interface/Simple1DCanvas.hpp"
 
-void
-SiPMNonLinearFit::InitDefaults()
-{
-  _lin_pmax  = 0;
-  _lin_pmin  = 0;
-  _power_z   = 500;
-  _pixel_min = 1000;
-  _pixel_max = 7000;
-
-  _readout_units = "[A.U.]";
-  _setup         = "UMD Gantry";
-  _sipm_model    = "HDR2 (7500 pix.)";
-  _sipm_id       = "";
-}
+/**
+ * @class SiPMNonLinearFit
+ * @ingroup SiPMCalc
+ *
+ * @brief Fitting Routine class for SiPM nonlinearity.
+ *
+ * @details The class takes the function of the input of a the zscan command from
+ * the [control program][control-zscan], calculates the number of estimated
+ * incoming photons for each of the data rows collected via the inverse square
+ * estimate and a corrector file for the LED bias settings, to get a graph for
+ * the readout (in units of fired pixels) vs the estimated number of incoming
+ * photons. The output of this file can be used to investigate the nonlinearity
+ * between the light input and the entire signal output chain (SiPM and readout
+ * amplification/digitization chain) in the format of presentation ready plots.
+ *
+ * As in the final analysis chain when we are running bulk analysis over many,
+ * similarly parameterized SiPMs, certain inputs to the nonlinearity fit will
+ * only be available after addition analysis routines of files other than the
+ * output of the `zscan` command , these we leave as function input arguments.
+ * All other arugments shall be defined as program options so that they can
+ * either be specified with a UNIX-cfg file or via command line arguments. For
+ * details of these arguments, see the following methods:
+ *
+ * - `FitArguments()` : For augmenting how the data within the zscan data should
+ *   be manipulated.
+ * - `OutputArguments()` : For modifying the output plot display string.
+ *
+ * This class the responsible main for morphing the data according the inverse
+ * square law and some corrections for the LED bias to get the number of
+ * estimated photons for data row collected. A typically analysis routine will
+ * run the following functions in order:
+ *
+ * - `ReadFiles()`: Read the data collected via a `zscan` command, and some LED
+ *   bias correction file.
+ * - `MakeLinearGraph()`: Getting the luminosity multiplier as a function of the
+ *   data collection z cooridnates, assuming that for some small LED bias range
+ *   the SiPM is sufficient linear and the deviation from a perfect point light
+ *   source for the given data collection range is small.
+ * - `MakeNonLinearGraph()`: using the the geometric luminosity multiplier and
+ *   the LED bias corrector file, make the graph of a function of estimated
+ *   incoming photons vs the readout value.
+ * - `RunNonLinearFit()`: routine for calling the fitter and fitting the data to
+ *   the results.
+ *
+ * Look at each of the detailed documentation of the functions above to see how
+ * each step is done. In addition to the analysis functions, the various `Plot`
+ * methods can be called to get presentation ready-plots at each stage of the
+ * analysis chain.
+ *
+ * [control-zscan]:
+ * https://umdcms.github.io/SiPMCalibControl/classctlcmd_1_1motioncmd_1_1zscan.html
+ *
+ */
 
 
 SiPMNonLinearFit::SiPMNonLinearFit()
@@ -36,6 +77,28 @@ SiPMNonLinearFit::SiPMNonLinearFit( const std::string& config )
 }
 
 
+/**
+ * @brief Arguments for updating how the linear and nonlinear part of the fit
+ * should be augmented.
+ *
+ * The allow options are:
+ *
+ * - `linpmin`, `linpmax`: These are the bias value limits of where the SiPM
+ *   output is expected to be sufficiently linear and the readout should follow a
+ *   near-perfect inverse square with the data collection z value (with z-offset
+ *   and pedestal of course.) Notice that these values should effective bracket a
+ *   single pwm duty cycle settings used in the zscan command, except that the
+ *   the bias values would have a finite range due to DC drifts in the system.
+ * - `powerz`: In the case the a bias correction file is not provided. one can be
+ *   generated on the fly using the readout values of data points closest to the
+ *   z value provided by this option. Notice that because this method lacks
+ *   proper pedestal subtraction, the output of this method can potentially be
+ *   wrong. Notice this will be ignored regardless of input value if a bias
+ *   corrector file was provided for reading.
+ * - `npixels`: The number of pixels to use in the fit. This can either be a
+ *   fixed value (in which case, simply use this options with a single input) or
+ *   can be used to indicate the range of the fit.
+ */
 usr::po::options_description
 SiPMNonLinearFit::FitArguments()
 {
@@ -49,17 +112,29 @@ SiPMNonLinearFit::FitArguments()
     "Maximum power level for linear power level fit" )
     ( "powerz",
     usr::po::value<double>(),
-    "z value to use for power level look up, will be ignored if a correction file is provided" )
-
+    "z value to use for power level look up, (ignored if a correction file is provided)" )
     ( "npixels",
     usr::po::multivalue<double>(),
-    "The number of pixels in the SiPM of interest, you can either enter a single number so that "
-    "this number is fixed in during the fitting, or 2 numbers to indicate the range." )
+    "The number of pixels in the SiPM of interest" )
   ;
   return desc;
 }
 
 
+/**
+ * @brief Options for modifying the output plots
+ *
+ * @details The following options are implemented:
+ * - `units` The string to be placed in the units of the y axis in the non-linear
+ *   plot.
+ * - `setup` The string to be placed in the top right of the pad (where the
+ *   luminosity data is usually placed in CMS plots)
+ * - `sipmmodel` The string representing the SiPM model (written underneath the
+ *   "HGCAL" standard line at the top left of the canvase)
+ * - `sipmid` The string representing the unique ID of the SiPM (written
+ *   underneath the SiPM model string.)
+ *
+ */
 usr::po::options_description
 SiPMNonLinearFit::OutputArguments()
 {
@@ -80,7 +155,28 @@ SiPMNonLinearFit::OutputArguments()
 
 
 /**
- * @brief Updating the results according to the input arguments.
+ * @brief  Initializing the setting parameters
+ *
+ */
+void
+SiPMNonLinearFit::InitDefaults()
+{
+  _lin_pmax  = 0;
+  _lin_pmin  = 0;
+  _power_z   = 500;
+  _pixel_min = 1000;
+  _pixel_max = 7000;
+
+  _readout_units = "[A.U.]";
+  _setup         = "UMD Gantry";
+  _sipm_model    = "HDR2 (7500 pix.)";
+  _sipm_id       = "";
+}
+
+
+/**
+ * @brief Updating the fitting routine parameters according to the input
+ * arguments.
  */
 void
 SiPMNonLinearFit::UpdateSettings( const usr::ArgumentExtender& args )
@@ -107,8 +203,12 @@ SiPMNonLinearFit::UpdateSettings( const usr::ArgumentExtender& args )
 
 
 /**
- * @brief Reading the raw data file and the data correction file, storing
- * results into the class.
+ * @brief Reading the raw data file and the data correction file, storing results
+ * into the class.
+ *
+ * At this point no data manipulation is used. Only data correction is made. For
+ * details on how the corrector file is handled, see the MakeInterpolator3D()
+ * method.
  */
 void
 SiPMNonLinearFit::ReadFiles( const std::string& zscan, const std::string& corr )
@@ -128,16 +228,16 @@ SiPMNonLinearFit::ReadFiles( const std::string& zscan, const std::string& corr )
 }
 
 
-void SiPMNonLinearFit::PlotLinearity( const std::string& ) {}
-
-
 /**
- * @brief In the case that the external correction table is not available.
- * Create a simplified lookup table using correcting data readout values.
+ * @brief In the case that the external correction table is not available. Create
+ * a simplified lookup table using correcting data readout values.
  *
  * The look-up table here is generated using the data collection values at the
- * maximum z value, and will only be a correction value for the bias voltage.
- * (temperature drift correction will be omitted)
+ * specified z value, and will only be a correction value for the bias voltage.
+ * (temperature drift correction will be omitted). Notice that this method
+ * effectively ignores any sort of imperfect pedestal subtraction results. and
+ * thus can be inaccurate, but works well for a quick and dirty evaluation of
+ * results.
  */
 void
 SiPMNonLinearFit::MakeInterpolator1D()
@@ -160,10 +260,10 @@ SiPMNonLinearFit::MakeInterpolator1D()
   readout.push_back( readout.back() );
 
   // Making the 1D interpolator
-  _lookup_1d.reset( new ROOT::Math::Interpolator( bias,
-                                                  readout,
-                                                  ROOT::Math::Interpolation::
-                                                  kLINEAR ));
+  _lookup_1d.reset(
+    new ROOT::Math::Interpolator( bias,
+                                  readout,
+                                  ROOT::Math::Interpolation::kLINEAR ));
 }
 
 
@@ -171,10 +271,36 @@ SiPMNonLinearFit::MakeInterpolator1D()
  * @brief Creating the data correction lookup table from an external correction
  * file.
  *
- * TODO: Properly implement
+ * The corrector file is a file containing numbers in 2 columns, the first
+ * columns is the LED bias value, the second number is the relative luminosity
+ * output (in arbitrary units) of the corresponding LED bias value (assuming 0
+ * temperature drift for now). All values are then used to construct the
+ * interpolator.
  */
-void SiPMNonLinearFit::MakeInterpolator3D( const std::string& corr )
-{}
+void
+SiPMNonLinearFit::MakeInterpolator3D( const std::string& corr )
+{
+  std::ifstream infile( corr );
+  std::string   line;
+
+  std::vector<double> bias;
+  std::vector<double> readout;
+
+  while( std::getline( infile, line ) ){
+    std::istringstream linestream( line );
+    double             bias_value;
+    double             readout_value;
+    linestream >> bias_value >> readout_value;
+    bias.push_back( bias_value );
+    readout.push_back( readout_value );
+  }
+
+  _lookup_1d.reset(
+    new ROOT::Math::Interpolator( bias,
+                                  readout,
+                                  ROOT::Math::Interpolation::kLINEAR ));
+}
+
 
 /**
  * @brief Making the linear graph as well as fitting to the inverse square law
@@ -226,11 +352,28 @@ SiPMNonLinearFit::MakeLinearGraph()
 
 /**
  * @brief Creating the graph for the number of expected photons (x axis) vs the
- * number of fired pixels as seen in the readout given the reference point for
- * a number of given photons.
+ * number of fired pixels as seen in the readout given the reference point for a
+ * number of given photons.
  *
- * As the reference point might not exist perfectly, here we find the closest
- * point.
+ * The function expects the following inputs, the ref_n, ref_z, and ref_bias
+ * refers to that for this SiPM, we expect ref_n effective photons for when the
+ * SiPM is at position ref_z and LED bias value ref_bias. As ref_n is going to be
+ * obtain by a low-light spectral fit (external to the data collected via the
+ * zscan data), these values need to be provided externally as function inputs.
+ * As bias voltages can drift between the zscan command the data collection for a
+ * low light spectrum, the closest value data row will be used as the "reference
+ * row".
+ *
+ * Once the reference row has been determined, the photon multiplier according to
+ * the z position (via the fit to inverse square) and the bias/temperature drifts
+ * via the look up table, can be used to morph the data according to the two
+ * multipliers to get the number of incoming photons vs the readout value.
+ *
+ * In addition to the 3 inputs above, the gain and the pedestal should also be
+ * given to this function to further reduce the readout value to be equivalent to
+ * the average number of pixels fired. One can of course, opt to leave these two
+ * values at 1 and 0 if one wants to compare the raw readout value v.s. the
+ * number of incoming photons.
  */
 void
 SiPMNonLinearFit::MakeNonLinearGraph( const double ref_n,
@@ -248,7 +391,7 @@ SiPMNonLinearFit::MakeNonLinearGraph( const double ref_n,
     if( fabs( l.z-ref_z ) != fabs( r.z-ref_z ) ){
       return fabs( l.z-ref_z ) < fabs( r.z-ref_z );
     } else {
-      return fabs( l.bias-ref_bias ) < fabs( r.z-ref_bias );
+      return fabs( l.bias-ref_bias ) < fabs( r.bias-ref_bias );
     }
   } );
 
@@ -258,6 +401,9 @@ SiPMNonLinearFit::MakeNonLinearGraph( const double ref_n,
   std::vector<double> out_unc;
 
   // Looping over all data
+
+  // TODO: the positional estimator should also include uncertainties. Find a
+  // good way to properly propagate this.
   for( const auto row : *_raw_data ){
     const double z_mult = GetZPosMultiplier( row, ref_row, ped );
     const double l_mult = GetLookUpMultiplier( row,  ref_row );
@@ -277,8 +423,7 @@ SiPMNonLinearFit::MakeNonLinearGraph( const double ref_n,
 
 
 /**
- * @brief Running the non linear fit. Definition of the SiPM will be determined by
- *
+ * @brief Running the non linear fit.
  *
  */
 void
@@ -312,7 +457,116 @@ SiPMNonLinearFit::RunNonLinearFit()
 
 
 /**
- * @brief Ploting the fit results of the zscan data used for inverse square estimation.
+ * @brief Generating the plot for the inverse square fit.
+ *
+ */
+void
+SiPMNonLinearFit::PlotLinearity( const std::string& outfile )
+{
+  usr::plt::Ratio1DCanvas c;
+
+  auto& fitg = c.PlotFunc(
+    _lin_func,
+    usr::plt::PlotType( usr::plt::fittedfunc ),
+    usr::plt::EntryText(  "Fitted Data" ),
+    usr::plt::VisualizeError( _lin_fit, 1, false ),
+    usr::plt::FillColor( usr::plt::col::cyan ),
+    usr::plt::TrackY( usr::plt::tracky::max ),
+    usr::plt::LineColor( usr::plt::col::blue ));
+  auto& datag = c.PlotGraph(
+    _lin_data,
+    usr::plt::PlotType( usr::plt::scatter ),
+    usr::plt::EntryText( "Readout" ),
+    usr::plt::MarkerColor(  usr::plt::col::black ),
+    usr::plt::MarkerStyle(  usr::plt::sty::mkrcircle ),
+    usr::plt::MarkerSize( 0.2 ),
+    usr::plt::LineColor( usr::plt::col::gray, 1.0 ) );
+  c.PlotScale(
+    fitg,
+    fitg,
+    usr::plt::PlotType( usr::plt::fittedfunc ),
+    usr::plt::FillColor( usr::plt::col::cyan ) );
+  c.PlotScale(
+    datag,
+    fitg,
+    usr::plt::PlotType( usr::plt::scatter ),
+    usr::plt::MarkerSize( 0.2 ) );
+
+  c.TopPad().Yaxis().SetTitle( "Luminosity - Ped. [mV-ns]" );
+  c.BottomPad().Xaxis().SetTitle( "Gantry z + z_{0} [mm]" );
+  c.BottomPad().Yaxis().SetTitle( "Data/Fit" );
+
+  c.TopPad().SetLogy( kTRUE );
+  c.TopPad().SetLogx( kTRUE );
+  c.TopPad().SetYaxisMin( usr::plt::GetYmin( _lin_data ) / 30  );
+  c.BottomPad().SetLogx( kTRUE );
+
+  c.DrawLuminosity( "LED setup" );
+  c.DrawCMSLabel( "Preliminary", "HGCal" );
+  c.TopPad()
+  .SetTextCursor( c.TopPad().InnerTextLeft(), 0.45 )
+  .WriteLine( "#frac{L_{0}(z+z_{0})}{(x_{0}^{2} + (z+z_{0})^{2})^{3/2}} + Ped" );
+  c.TopPad()
+  .SetTextCursor( c.TopPad().InnerTextLeft(), 0.25 )
+  .WriteLine( usr::fstr( "Fit Ped = %.2lf_{#pm%.3lf}",
+                         _lin_func.GetParameter( 3 ),
+                         _lin_func.GetParError( 3 ) ) )
+  .WriteLine( usr::fstr( "Fit z_{0} = %.2lf_{#pm%.3lf}",
+                         -_lin_func.GetParameter( 0 ),
+                         _lin_func.GetParError( 0 ) ) )
+  .WriteLine( usr::fstr( "Fit x_{0} = %.2lf_{#pm%.3lf}",
+                         _lin_func.GetParameter( 1 ),
+                         _lin_func.GetParError( 1 ) ) )
+  .WriteLine( usr::fstr( "#chi^{2}/D.o.F = %.2lf/%d",
+                         _lin_fit.Chi2(),
+                         _lin_fit.Ndf() ) );
+
+  c.SaveAsPDF( outfile );
+}
+
+
+/**
+ * @brief  Plotting the original data plots with the various bias voltages
+ * represented using different color points.
+ *
+ * Notice that this functions needs to be ran *after* the linear fit has been
+ * ran, as we will need the z off set subtraction to avoid unnecessary confusions
+ * when interpreting the results of the plots.
+ */
+void
+SiPMNonLinearFit::PlotOriginal( const std::string& outfile )
+{
+  usr::plt::Flat2DCanvas c;
+
+  auto zval    = _raw_data->Z();
+  auto readout = _raw_data->DataCol( 0 );
+  auto bias    = _raw_data->Bias();
+
+  std::transform( zval.begin(),
+                  zval.end(),
+                  zval.begin(), [this]( const double z )->double {
+    return z-this->_lin_func.GetParameter( 0 );
+  } );
+
+  TGraph2D g( zval.size(), zval.data(), readout.data(), bias.data() );
+
+  c.PlotColGraph( g,
+                  usr::plt::MarkerSize( 0.5 ),
+                  usr::plt::MarkerStyle( usr::plt::sty::mkrcircle ) );
+  c.Pad().Xaxis().SetTitle( "Gantry coordinates" );
+  c.Pad().Yaxis().SetTitle( "Readout value" );
+  c.Pad().Zaxis().SetTitle( "LED Bias [mV]" );
+  c.Pad().DrawLuminosity( _setup );
+  c.Pad().DrawCMSLabel( "", "HGCAL" );
+  c.Pad().SetLogx( kTRUE );
+  c.Pad().SetLogy( kTRUE );
+  c.SaveAsPDF( outfile );
+}
+
+
+/**
+ * @brief Ploting the fit results of the zscan data used for inverse square
+ * estimation.
  */
 void
 SiPMNonLinearFit::PlotNonLinearity( const std::string& outfile )
@@ -326,13 +580,13 @@ SiPMNonLinearFit::PlotNonLinearity( const std::string& outfile )
     usr::plt::EntryText( "Fit" ),
     usr::plt::LineColor( usr::plt::col::blue, 1.0 ),
     usr::plt::FillColor( usr::plt::col::cyan, 1.0 ),
-    RooFit::Precision( 0.0001 ),
+    usr::plt::Precision( 0.001, true ),
     usr::plt::TrackY( usr::plt::tracky::max ));
   auto& datag = c.PlotGraph(
     _nl_data,
     usr::plt::PlotType( usr::plt::scatter ),
     usr::plt::EntryText( "Data" ),
-    usr::plt::TrackY( usr::plt::tracky::max ),
+    usr::plt::TrackY( usr::plt::tracky::both ),
     usr::plt::MarkerColor(  usr::plt::col::black ),
     usr::plt::MarkerStyle(  usr::plt::sty::mkrcircle ),
     usr::plt::MarkerSize( 0.2 ),
@@ -366,7 +620,13 @@ SiPMNonLinearFit::PlotNonLinearity( const std::string& outfile )
 
 
 /**
- * @brief TODO: UPDATE DOCUMENTATION
+ * @brief Getting the photon multiplier factor based on the z coordinates
+ * relative to some reference point.
+ *
+ * Ths function will only ever the use z cooridnates of the data rows of
+ * interest. In addition, the use can provide a custom pedestal value which will
+ * make it so that the pedestal value evaluated from the inverse square fit is
+ * ignored.
  */
 double
 SiPMNonLinearFit::GetZPosMultiplier( const StdFormat::RowFormat& main,
@@ -398,6 +658,7 @@ SiPMNonLinearFit::GetLookUpMultiplier( const StdFormat::RowFormat& main,
                                        const StdFormat::RowFormat& ref ) const
 {
   if( _lookup_3d != nullptr ){
+    //TODO: implement the temperature compensation version of this look up table.
     return 0;
   } else {
     return _lookup_1d->Eval( main.bias ) / _lookup_1d->Eval( ref.bias );
